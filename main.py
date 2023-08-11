a2899e1634fc4d2f9b64a048c034f13939f87790
RockChinQ <1010553892@qq.com>
2023-08-11 21:34:23 +0800

    feat: 基本功能

--- main.py
+++ main.py
@@ -1,49 +1,65 @@
+import os
+import shutil
+import json
+
+import yaml
+import requests
+
 from pkg.plugin.models import *
 from pkg.plugin.host import EventContext, PluginHost
 
-"""
-在收到私聊或群聊消息"hello"时，回复"hello, <发送者id>!"或"hello, everyone!"
-"""
-
 
 # 注册插件
-@register(name="Hello", description="hello world", version="0.1", author="RockChinQ")
-class HelloPlugin(Plugin):
+@register(name="AnsweringPlugin", description="使用RockChinQ/llm-embed-qa作为后端，回答特定领域问题", version="0.1", author="RockChinQ")
+class AnsweringPlugin(Plugin):
+
+    cfg: dict
 
     # 插件加载时触发
     # plugin_host (pkg.plugin.host.PluginHost) 提供了与主程序交互的一些方法，详细请查看其源码
     def __init__(self, plugin_host: PluginHost):
-        pass
-
-    # 当收到个人消息时触发
-    @on(PersonNormalMessageReceived)
-    def person_normal_message_received(self, event: EventContext, **kwargs):
-        msg = kwargs['text_message']
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            logging.debug("hello, {}".format(kwargs['sender_id']))
-
-            # 回复消息 "hello, <发送者id>!"
-            event.add_return("reply", ["hello, {}!".format(kwargs['sender_id'])])
-
-            # 阻止该事件默认行为（向接口获取回复）
-            event.prevent_default()
-
-    # 当收到群消息时触发
+        # 检查是否有answer.yaml文件
+        if not os.path.exists("answer.yaml"):
+            shutil.copyfile("plugins/AnsweringPlugin/config-template.yaml", "answer.yaml")
+            logging.info("AnsweringPlugin: 未找到answer.yaml文件，已自动创建，请修改后重启程序")
+            return
+        
+        # 读取answer.yaml文件
+        with open("answer.yaml", "r", encoding="utf-8") as f:
+            self.cfg = yaml.load(f, Loader=yaml.FullLoader)
+        
     @on(GroupNormalMessageReceived)
-    def group_normal_message_received(self, event: EventContext, **kwargs):
-        msg = kwargs['text_message']
-        if msg == "hello":  # 如果消息为hello
-
-            # 输出调试信息
-            logging.debug("hello, {}".format(kwargs['sender_id']))
-
-            # 回复消息 "hello, everyone!"
-            event.add_return("reply", ["hello, everyone!"])
-
-            # 阻止该事件默认行为（向接口获取回复）
+    @on(PersonNormalMessageReceived)
+    def _(self, event: EventContext, **kwargs):
+        content = kwargs['text_message']
+        
+        resp = requests.get(
+            url=self.cfg['endpoint']+"/ask",
+            params={
+                "content": content,
+                "strict": 'true'
+            },
+            timeout=300
+        )
+        
+        json_data = json.loads(resp.text)
+        
+        if len(json_data['doc_names']) > 0:
             event.prevent_default()
+            event.prevent_postorder()
+            
+            event.add_return(
+                "reply",
+                [json_data['message']]
+            )
+        else: # 没有找到相关资料
+            if self.cfg['skip_if_not_found']:
+                event.add_return(
+                    "reply",
+                    ["没有找到与问题相关的资料哦"]
+                )
+                event.prevent_default()
+                event.prevent_postorder()
 
     # 插件卸载时触发
     def __del__(self):
